dabdb0499317593dfec9082517a94dcf8375a320
AleDore <alessio85.dore@gmail.com>
2022-07-14 13:55:08 +0200

    add workloads and refactor

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# io-payment-updater-load-test
+# Load tests for IO-Payment-updater project
+
+This is a set of [k6](https://k6.io) load tests related to the Payment Updater component.
+
+All tests require a set of parameters: **rate**, **duration**, **preAllocatedVUs** and **maxVUs**. These parameters are necessary to set the test target to a given number of iteration per second (**rate**) in a given time (**duration**), using a certain number of VUs (**preAllocatedVUs** and **maxVUs**).
+
+To invoke k6 load test passing parameter use -e (or --env) flag:
+```
+-e MY_VARIABLE=MY_VALUE
+```
+
+
+## 01. New Payment Producer
+
+This test produce a Payment
+
+```
+$ docker run -i --rm -v $(pwd)/src:/src  -e PRODUCER_BASE_URL=${PRODUCER_BASE_URL} -e rate=${rate} -e duration=${duration} -e preAllocatedVUs=${preAllocatedVUs} -e maxVUs=${maxVUs} loadimpact/k6 run /src/producer_newpayment.js
+```
+
+## 02. Payment check.
+
+Payment check test
+
+```
+$ docker run -i --rm -v $(pwd)/src:/src  -e PU_BASE_URL=${PU_BASE_URL} -e rate=${rate} -e duration=${duration} -e preAllocatedVUs=${preAllocatedVUs} -e maxVUs=${maxVUs} loadimpact/k6 run /src/payment_check.js
+```
+
+# Full workloads
+
+## 01. Payment with Related Messages
+
+This test represents payment messages lifecycle through Payment Updater.
+
+```
+$ docker run -i --rm -v $(pwd)/src:/src -e PRODUCER_BASE_URL=${PRODUCER_BASE_URL} -e PU_BASE_URL=${PU_BASE_URL} -e rate=${rate} -e duration=${duration} -e preAllocatedVUs=${preAllocatedVUs} -e maxVUs=${maxVUs} loadimpact/k6 run /src/message_payments.js
+```
---
+++ src/producer_newpayment.js
@@ -0,0 +1,105 @@
+import { check, sleep } from 'k6';
+import http from 'k6/http';
+
+export let options = {
+    scenarios: {
+        contacts: {
+            executor: 'constant-arrival-rate',
+            rate: __ENV.rate, // e.g. 20000 for 20K iterations
+            duration: __ENV.duration, // e.g. '1m'
+            preAllocatedVUs: __ENV.preAllocatedVUs, // e.g. 500
+            maxVUs: __ENV.maxVUs // e.g. 1000
+        }
+    },
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        'http_req_duration{Method:newPaymentKafka}': ['p(95)<1000'], // threshold on API requests only
+    },
+};
+
+const payload = JSON.stringify({
+	uuid: '',
+	version: '1',
+	idPaymentManager:'15735423',
+	complete: 'false',
+	missingInfo: [
+		'missingInfo1',
+		'missingInfo2'
+	],
+	debtorPosition : {
+		modelType: '2',
+		noticeNumber: 'A10',
+		iuv: '97735020584'
+	},
+	creditor : {
+		idPA: 'undefined',
+		idBrokerPA: '77777777777',
+		idStation: '77777777777_01',
+		companyName: 'company EC'
+	},
+	psp : {
+		idPsp : '88888888888',
+		idBrokerPsp: '88888888888',
+		idChannel: '88888888888_01',
+		psp: 'PSP'
+	},
+	debtor: {
+		fullName : 'John Doe',
+		entityUniqueIdentifierType: 'F',
+		entityUniqueIdentifierValue: 'JHNDOE00A01F205N'
+	},
+	payer: {
+		fullName : 'John Doe',
+		entityUniqueIdentifierType: 'F',
+		entityUniqueIdentifierValue: 'JHNDOE00A01F205N'
+	},
+	paymentInfo: {
+		paymentDateTime: '2022-01-24',
+		applicationDate: '2022-01-24',
+		transferDate: '2022-01-24',
+		dueDate: '2022-01-24',
+		paymentToken: 'c110729d258c4ab1b765fe902aae41d6',
+		amount: '110',
+		fee: '1.00',
+		totalNotice : '1',
+		paymentMethod : 'creditCard',
+		touchpoint : 'IO',
+		remittanceInformation : 'reason for payment'
+	},
+	transferList : [
+		{
+			fiscalCodePA : '77777777777',
+			companyName : 'company EC',
+			amount : '100',
+			transferCategory : '0101100IM',
+			remittanceInformation : 'remittanceInformation2'
+		},
+		{
+			fiscalCodePA : '77777777778',
+			companyName : 'company EC1',
+			amount : '10',
+			transferCategory : '0201102IM',
+			remittanceInformation : 'remittanceInformation3'
+		}
+	]
+});
+
+const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  };
+
+export default function() {
+	// Values from env var.
+    var producerBaseUrl = `${__ENV.PRODUCER_BASE_URL}`
+	var tag = {
+        Method: "newPaymentKafka",
+    };
+	let url = `${producerBaseUrl}/notifications/newPaymentKafka`;
+	let res = http.post(url, payload, params, {
+        tags: tag,
+    });
+	check(res, { 'status was 200' : r => r.status == 200 });
+	sleep(1);
+}
